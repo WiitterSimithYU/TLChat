eddd0aff13be0a21701671ecbf36fe665886833b
WiitterSimithYU <378961614@qq.com>
2017-12-22 17:50:23 +0800

    commit

---
+++ TLChat/SRC/Chat/ChatViewController/TLChatViewController.h
@@ -0,0 +1,14 @@
+//
+//  TLChatViewController.h
+//  TLChat
+//
+//  Created by 李伯坤 on 16/2/15.
+//  Copyright © 2016年 李伯坤. All rights reserved.
+//
+
+#import "TLChatBaseViewController.h"
+
+@interface TLChatViewController : TLChatBaseViewController
+
+
+@end
